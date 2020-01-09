bc1dd783fe445bf9e9b3bb7a1dbde392625e6f9b
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-09 22:24:00 +0530

    parsing the JSON

--- repository/users.js
+++ repository/users.js
@@ -1,17 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class UserRepository {
   constructor(filename) {
     if (!filename) {
-      throw new Error('Creating a repository require a filename');
+      throw new Error("Creating a repository require a filename");
     }
     this.filename = filename;
     try {
       fs.accessSync(this.filename);
     } catch (err) {
-      fs.writeFileSync(this.filename, '[]');
+      fs.writeFileSync(this.filename, "[]");
     }
+  }
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8"
+      })
+    );
   }
 }
 
-new UserRepository('user.json');
+const test = async () => {
+  const repo = new UserRepository("user.json");
+  const user = await repo.getAll();
+
+  console.log(user);
+};
+
+test();
